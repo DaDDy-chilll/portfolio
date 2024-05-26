8a7eac9c2403f4e6d8208451f6ac1b2624ccfa10
DaDDy <blacklistuser420@gmail.com>
2024-05-26 11:39:48 +0630

    update

--- server/controllers/httpGetCV.js
+++ server/controllers/httpGetCV.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-undef */
 const path = require("path");
 const httpGetCV = async (req, res) => {
-try {
-    const filePath = path.join(__dirname, "..", "assets", "PAING-SETT-KYAW.pdf");
-    res.download(filePath)
-    console.log('succss downloaded');
-} catch (error) {
+  try {
+    const filePath = path.join(
+      __dirname,
+      "..",
+      "assets",
+      "PAING-SETT-KYAW.pdf"
+    );
+    res.download(filePath);
+    console.log("succss downloaded");
+  } catch (error) {
     console.log(error.message);
     res.status(400).json(error.message);
-}
+  }
 };
 
 module.exports = httpGetCV;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,19 @@
 /* eslint-disable no-undef */
-const http = require('http')
-const express = require('express');
-const cors = require('cors')
-const morgan = require('morgan')
-const api = require('./routes/api');
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const api = require("./routes/api");
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const PORT = 3000;
 
+app.use(cors({ origin: "http://localhost:5173", methods: ["GET"] }));
+app.use(morgan("dev"));
 
+app.use("/api/v1", api);
+app.get("*", (req, res) => {
+  res.send("route doesn't defined.");
+});
 
-
-app.use(cors(
-    {origin:'http://localhost:5173',methods:['GET']}
-))
-app.use(morgan( 'dev'));
-
-app.use('/api/v1',api)
-app.get('*',(req,res) => {
-    res.send('route doesn\'t defined.')
-})
-
-server.listen(PORT,()=> console.log(`server is running on port:${PORT}`))
+server.listen(PORT, () => console.log(`server is running on port:${PORT}`));
