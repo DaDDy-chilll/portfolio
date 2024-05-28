92e1523fda7f03a1c25fb336e9bdb44f13541f9e
DaDDy <blacklistuser420@gmail.com>
2024-05-28 19:46:19 +0630

    update

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -3,6 +3,7 @@ const { createMail } = require("./createMail");
 
 const sendMail = (user) => {
   const mail = createMail();
+  //! Mail Template
   const mailTemplate = `
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
