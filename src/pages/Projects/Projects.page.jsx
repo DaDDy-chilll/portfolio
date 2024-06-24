b69000087fc3f47f825d3f9244ea2fc08294238a
DaDDy <blacklistuser420@gmail.com>
2024-06-24 12:56:30 +0630

    update

--- src/pages/Projects/Projects.page.jsx
+++ src/pages/Projects/Projects.page.jsx
@@ -2,6 +2,28 @@
 import { motion } from "framer-motion";
 import { Container, BackGround } from "./Projects.style";
 
+const projects = [
+  {
+    id: 1,
+    name: "shoppie",
+    image: "shoppie",
+    github: "https://github.com/who-0/shoppie",
+    live: "https://shoppie-gqvo.onrender.com/",
+    language: ["react", "node", "express", "mongodb"],
+    description: "Mini online shop website.",
+    explain:""
+  },
+  {
+    id: 2,
+    name: "robot friends",
+    image: "robot",
+    github: "https://github.com/DaDDy-chilll/myrobotsfriend",
+    live: "https://newsletter-c3an.onrender.com/",
+    language: ["react", "Robots API"],
+    description: "Get json data from Robots Api and display with React",
+  },
+];
+
 const Projects = () => {
   return (
     <>
